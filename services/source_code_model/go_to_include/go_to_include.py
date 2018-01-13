0647830bd58e85b8db74221ebc5455fcdb3d5724
Jusufadis Bakamovic <jbakam@gmail.com>
2018-01-13 10:08:21 +0100

    Make go-to-include service return None if include is not found.

--- services/source_code_model/go_to_include/go_to_include.py
+++ services/source_code_model/go_to_include/go_to_include.py
@@ -3,15 +3,12 @@ class GoToInclude():
         self.parser = parser
 
     def __call__(self, args):
-        original_filename = str(args[0])
-        contents_filename = str(args[1])
-        line              = int(args[2])
-
-        include_filename = ''
+        include_filename = None
+        original_filename, contents_filename, line = args
         tunit = self.parser.parse(contents_filename, original_filename)
         for include in self.parser.get_top_level_includes(tunit):
             filename, l, col = include
             if l == line:
                 include_filename = filename
                 break
-        return (tunit != None and include_filename != ''), include_filename
+        return (tunit != None and include_filename != None), include_filename
