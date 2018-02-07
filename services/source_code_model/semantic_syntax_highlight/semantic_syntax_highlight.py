54a7d38a3bf608ebe0058e7cb17f7c00a283a5d3
Jusufadis Bakamovic <jbakam@gmail.com>
2018-02-07 20:24:46 +0100

    Move semantic syntax highlight visitor impl. out of the class. Impl. more unit tests.

--- services/source_code_model/semantic_syntax_highlight/semantic_syntax_highlight.py
+++ services/source_code_model/semantic_syntax_highlight/semantic_syntax_highlight.py
@@ -6,27 +6,8 @@ class SemanticSyntaxHighlight():
     def __init__(self, parser):
         self.parser = parser
 
-    def __traverse__(self, tunit, callback, data):
-        def visitor(ast_node, ast_parent_node, data):
-            if ast_node.location.file and ast_node.location.file.name == tunit.spelling:  # we're only interested in symbols from associated translation unit
-                ast_node_id = self.parser.get_ast_node_id(ast_node)
-                if ast_node_id != ASTNodeId.getUnsupportedId():
-                    callback(
-                        ast_node_id,
-                        self.parser.get_ast_node_name(ast_node),
-                        self.parser.get_ast_node_line(ast_node),
-                        self.parser.get_ast_node_column(ast_node),
-                        data
-                    )
-                else:
-                    logging.debug("Unsupported token id: [{0}, {1}]: {2} '{3}'".format(
-                            ast_node.location.line, ast_node.location.column,
-                            ast_node.kind, ast_node.spelling
-                        )
-                    )
-                return ChildVisitResult.RECURSE.value  # If we are positioned in TU of interest, then we'll traverse through all descendants
-            return ChildVisitResult.CONTINUE.value  # Otherwise, we'll skip to the next sibling
-        self.parser.traverse(tunit.cursor, data, visitor)
+    def __traverse__(self, tunit, callback, client_data):
+        self.parser.traverse(tunit.cursor, (self.parser, tunit.spelling, callback, client_data), semantic_syntax_highlight_visitor)
 
     def __call__(self, args):
         original_filename = str(args[0])
@@ -34,3 +15,25 @@ class SemanticSyntaxHighlight():
 
         tunit = self.parser.parse(contents_filename, original_filename)
         return tunit != None, [tunit, self.__traverse__]
+
+def semantic_syntax_highlight_visitor(ast_node, ast_parent_node, data):
+    parser, tunit_spelling, client_callback, client_data = data
+    if ast_node.location.file and ast_node.location.file.name == tunit_spelling:  # we're only interested in symbols from associated translation unit
+        ast_node_id = parser.get_ast_node_id(ast_node)
+        if ast_node_id != ASTNodeId.getUnsupportedId():
+            client_callback(
+                ast_node_id,
+                parser.get_ast_node_name(ast_node),
+                parser.get_ast_node_line(ast_node),
+                parser.get_ast_node_column(ast_node),
+                client_data
+            )
+        else:
+            logging.debug("Unsupported token id: [{0}, {1}]: {2} '{3}'".format(
+                    ast_node.location.line, ast_node.location.column,
+                    ast_node.kind, ast_node.spelling
+                )
+            )
+        return ChildVisitResult.RECURSE.value  # If we are positioned in TU of interest, then we'll traverse through all descendants
+    return ChildVisitResult.CONTINUE.value  # Otherwise, we'll skip to the next sibling
+
