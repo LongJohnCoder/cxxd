b9627d0b371ab808e6690177ddf37835361a8e32
Jusufadis Bakamovic <jbakam@gmail.com>
2018-02-07 18:28:21 +0100

    Replace 'assert_called_with' with 'assert_called_once_with' in service unit tests.

--- tests/test_service.py
+++ tests/test_service.py
@@ -11,17 +11,17 @@ class ServiceTest(unittest.TestCase):
     def test_if_send_startup_request_enqueues_correct_data(self):
         with mock.patch.object(self.service.queue, 'put') as mock_queue_put:
             self.service.send_startup_request(self.payload)
-        mock_queue_put.assert_called_with([0x0, self.payload])
+        mock_queue_put.assert_called_once_with([0x0, self.payload])
 
     def test_if_send_shutdown_request_enqueues_correct_data(self):
         with mock.patch.object(self.service.queue, 'put') as mock_queue_put:
             self.service.send_shutdown_request(self.payload)
-        mock_queue_put.assert_called_with([0x1, self.payload])
+        mock_queue_put.assert_called_once_with([0x1, self.payload])
 
     def test_if_send_request_enqueues_correct_data(self):
         with mock.patch.object(self.service.queue, 'put') as mock_queue_put:
             self.service.send_request(self.payload)
-        mock_queue_put.assert_called_with([0x2, self.payload])
+        mock_queue_put.assert_called_once_with([0x2, self.payload])
 
     def test_if_service_is_not_started_by_default(self):
         self.assertEqual(self.service.is_started_up(), False)
@@ -89,13 +89,13 @@ class ServiceTest(unittest.TestCase):
         self.service.send_startup_request(self.payload)
         with mock.patch.object(self.service, 'startup_callback') as mock_startup_callback:
             self.service.process_request()
-        mock_startup_callback.assert_called_with(self.payload)
+        mock_startup_callback.assert_called_once_with(self.payload)
 
     def test_if_send_startup_request_triggers_service_plugin_startup_callback(self):
         self.service.send_startup_request(self.payload)
         with mock.patch.object(self.service.service_plugin, 'startup_callback') as mock_startup_callback:
             self.service.process_request()
-        mock_startup_callback.assert_called_with(True, self.payload)
+        mock_startup_callback.assert_called_once_with(True, self.payload)
 
     def test_if_send_startup_request_triggers_service_startup_request_first_and_service_plugin_startup_request_second(self):
         manager = mock.MagicMock()
@@ -105,8 +105,8 @@ class ServiceTest(unittest.TestCase):
                 manager.attach_mock(mock_service_startup_callback, 'mock_service_startup_callback')
                 manager.attach_mock(mock_service_plugin_startup_callback, 'mock_service_plugin_startup_callback')
                 self.service.process_request()
-        mock_service_startup_callback.assert_called_with(self.payload)
-        mock_service_plugin_startup_callback.assert_called_with(True, self.payload)
+        mock_service_startup_callback.assert_called_once_with(self.payload)
+        mock_service_plugin_startup_callback.assert_called_once_with(True, self.payload)
         manager.assert_has_calls(
             [mock.call.mock_service_startup_callback(self.payload), mock.call.mock_service_plugin_startup_callback(True, self.payload)]
         )
@@ -117,7 +117,7 @@ class ServiceTest(unittest.TestCase):
         self.service.send_shutdown_request(self.payload)
         with mock.patch.object(self.service, 'shutdown_callback') as mock_shutdown_callback:
             self.service.process_request()
-        mock_shutdown_callback.assert_called_with(self.payload)
+        mock_shutdown_callback.assert_called_once_with(self.payload)
 
     def test_if_send_shutdown_request_triggers_service_plugin_shutdown_callback(self):
         self.service.send_startup_request(self.payload)
@@ -125,7 +125,7 @@ class ServiceTest(unittest.TestCase):
         self.service.send_shutdown_request(self.payload)
         with mock.patch.object(self.service.service_plugin, 'shutdown_callback') as mock_shutdown_callback:
             self.service.process_request()
-        mock_shutdown_callback.assert_called_with(True, self.payload)
+        mock_shutdown_callback.assert_called_once_with(True, self.payload)
 
     def test_if_send_shutdown_request_triggers_service_shutdown_request_first_and_service_plugin_shutdown_request_second(self):
         manager = mock.MagicMock()
@@ -137,8 +137,8 @@ class ServiceTest(unittest.TestCase):
                 manager.attach_mock(mock_service_shutdown_callback, 'mock_service_shutdown_callback')
                 manager.attach_mock(mock_service_plugin_shutdown_callback, 'mock_service_plugin_shutdown_callback')
                 self.service.process_request()
-        mock_service_shutdown_callback.assert_called_with(self.payload)
-        mock_service_plugin_shutdown_callback.assert_called_with(True, self.payload)
+        mock_service_shutdown_callback.assert_called_once_with(self.payload)
+        mock_service_plugin_shutdown_callback.assert_called_once_with(True, self.payload)
         manager.assert_has_calls(
             [mock.call.mock_service_shutdown_callback(self.payload), mock.call.mock_service_plugin_shutdown_callback(True, self.payload)]
         )
@@ -149,7 +149,7 @@ class ServiceTest(unittest.TestCase):
         self.service.send_request(self.payload)
         with mock.patch.object(self.service, '__call__', mock.Mock(return_value=(True, None))) as mock_service_request:
             self.service.process_request()
-        mock_service_request.assert_called_with(self.payload)
+        mock_service_request.assert_called_once_with(self.payload)
 
     def test_if_send_request_triggers_service_plugin_request(self):
         self.service.send_startup_request(self.payload)
@@ -158,7 +158,7 @@ class ServiceTest(unittest.TestCase):
         with mock.patch.object(self.service, '__call__', mock.Mock(return_value=(True, None))) as mock_service_request:
             with mock.patch.object(self.service.service_plugin, '__call__') as mock_service_plugin_request:
                 self.service.process_request()
-        mock_service_plugin_request.assert_called_with(True, self.payload, None)
+        mock_service_plugin_request.assert_called_once_with(True, self.payload, None)
 
     def test_if_send_request_triggers_service_request_first_and_service_plugin_request_second(self):
         manager = mock.MagicMock()
@@ -170,8 +170,8 @@ class ServiceTest(unittest.TestCase):
                 manager.attach_mock(mock_service_request, 'mock_service_request')
                 manager.attach_mock(mock_service_plugin_request, 'mock_service_plugin_request')
                 self.service.process_request()
-        mock_service_request.assert_called_with(self.payload)
-        mock_service_plugin_request.assert_called_with(True, self.payload, None)
+        mock_service_request.assert_called_once_with(self.payload)
+        mock_service_plugin_request.assert_called_once_with(True, self.payload, None)
         manager.assert_has_calls(
             [mock.call.mock_service_request(self.payload), mock.call.mock_service_plugin_request(True, self.payload, None)]
         )
